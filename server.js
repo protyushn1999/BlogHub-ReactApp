ba88d53d979ccbe1bdbc659e5f253dc2b3201b14
Protyush Nayak <protyushn@gmail.com>
2023-07-02 04:18:03 +0530

    [A]changes for netlify deployment

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("./db.json");
+// const middlewares = jsonServer.defaults({
+//   static: "./build",
+// });
+// const PORT = process.env.PORT || 9090;
+// server.use(middlewares);
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
+// server.use(router);
+// server.listen(PORT, () => {
+//   console.log("Server is running");
+// });
+
+// // Export the Server API
+// module.exports = server
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
+const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 9090;
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
-server.use(router);
-server.listen(PORT, () => {
-  console.log("Server is running");
-});
 
-// Export the Server API
-module.exports = server
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  "/api/*": "/$1"
+}));
+server.use(router);
+
+module.exports = server;
